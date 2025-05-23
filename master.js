41d64867e92e529c9278902f21cb733c5be5f938
mahmoud ahmed <mahmoud181712@gmail.com>
2025-05-24 02:48:40 +0300

    modified rate

--- master.js
+++ master.js
@@ -14,7 +14,7 @@ setInterval( ()=>{
 
 // place holder for result and rate 
  result[0].innerHTML="result will be  here"
-  result[1].innerHTML="daily rate for exhnage will be here"
+  result[1].innerHTML="daily rate for exchange will be here"
 //  object to collect data
 
 let data = {
